c06e7a7f9d7f07bccded6352462aca7ffd532dbe
Tazy432 <panzanihoratio@gmail.com>
2024-10-03 09:29:00 +0300

    modified main to download with loading bar , imported tqdm for loading bar and added to the requirements

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+from tqdm import tqdm  # Progress bar
+import os
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -7,14 +9,35 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
+    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",  # Invalid URL for testing
 ]
 
+def download_file(url):
+    # Get the file name
+    file_name = url.split('/')[-1]
+    
+    # Make the request with stream=True to download in chunks
+    response = requests.get(url, stream=True)
+    
+    if response.status_code != 404:
+        # Get the total file size from the headers (if available)
+        total_size = int(response.headers.get('content-length', 0))
+
+        # Open the file in binary write mode
+        with open(file_name, "wb") as file:
+            # Create a progress bar with tqdm
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc=file_name) as progress_bar:
+                # Write the content in chunks
+                for chunk in response.iter_content(chunk_size=1024):  # 1KB chunks
+                    if chunk:
+                        file.write(chunk)
+                        progress_bar.update(len(chunk))  # Update progress bar
+    else:
+        print(f"The zipfile {file_name} does not exist.")
 
 def main():
-    # your code here
-    pass
-
+    for url in download_uris:
+        download_file(url)
 
 if __name__ == "__main__":
     main()
